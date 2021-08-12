b204dba45739c1fe1c9f00c48646e8ef7365c60e
Noof <noof.mohammed998@gmail.com>
2021-08-12 19:56:17 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+$(".home-button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".main").offset().top},
+        'slow');
+});
+
+$(".about-button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".about").offset().top},
+        'slow');
+});
+
+$(".technical-button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".technical").offset().top},
+        'slow');
+});
+
+$(".personal-button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".personal").offset().top},
+        'slow');
+});
+
+$(".interests-button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".interests").offset().top},
+        'slow');
+});
+
+$(".projects-button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".projects").offset().top},
+        'slow');
+});
+
+$(".education-button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".education").offset().top},
+        'slow');
+});
+// $(".meraki").click( function() {
+//     $(".meraki").hide();
+    
+// });
